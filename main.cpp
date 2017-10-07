331cd6e6494e2db1d96dc562b4899db7f59b42c0
Bradley <classicbrad24@gmail.com>
2017-10-07 21:52:49 +1100

    Fixed map path

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@
 #include "Defines.h"
 
 int main() {
-  
+
   Console cTemp;
   WorldMap * wmap = new WorldMap;
   ActorManager amanager(TOTAL_ACTORS, wmap);
-  
+
   cTemp.Init();
   cTemp.PrintString("Loading Map!");
-  wmap->LoadMap("/home/pi/Documents/SourceCode/PathFinding/TestMap.txt");
+  wmap->LoadMap("TestMap.txt");
   cTemp.PrintString("Creating Actor!");
   wmap->DrawMap(&cTemp);
   cTemp.RefreshScreen();
   //cTemp.GetChar();
-  
+
   char c;
   while(c != 't') {
     amanager.SpawnActor();
@@ -28,7 +28,7 @@ int main() {
     wmap->DrawMap(&cTemp);
     amanager.DrawActors(&cTemp);
     cTemp.RefreshScreen();
-    
+
     c = cTemp.GetChar();
   }
   cTemp.Clean();
